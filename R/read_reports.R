2936bfbe923dc6f466a9eb3ad9d6213edefe87ab
Joseph Mohr <jmohr@thenightwatch.co>
2024-12-23 13:10:05 -0600

    Refactor `read_reports()` for better handling

--- R/read_reports.R
+++ R/read_reports.R
@@ -1,5 +1,7 @@
-read_reports <- function(path) {
-  lines <- readLines(path)
-  strsplit(lines, " ") %>%
-    lapply(as.integer)
+read_reports <- function(fPath) {
+  read_delim(fPath, col_names = FALSE) %>%
+    rowid_to_column("id") %>%
+    pivot_longer(!id, names_to = "level",
+                 values_to = "value",
+                 values_drop_na = TRUE )
 }
