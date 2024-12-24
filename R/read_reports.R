8c35b195905e91f4ab57ca5b11fcd353416153d9
Joseph Mohr <jmohr@thenightwatch.co>
2024-12-23 22:12:05 -0600

    sometimes, simpler is better...

--- R/read_reports.R
+++ R/read_reports.R
@@ -1,7 +1,7 @@
-read_reports <- function(fPath) {
-  read_delim(fPath, col_names = FALSE) %>%
-    rowid_to_column("id") %>%
-    pivot_longer(!id, names_to = "level",
-                 values_to = "value",
-                 values_drop_na = TRUE )
+read_reports <- function(path) {
+
+    lines <- readLines(path)
+    strsplit(lines, " ") %>%
+      lapply(as.integer)
+
 }
