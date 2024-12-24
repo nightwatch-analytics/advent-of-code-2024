4c6e4fd9adfdac493d7eee70c5e1320052072d2a
Joseph Mohr <jmohr@thenightwatch.co>
2024-12-23 22:22:56 -0600

    add problem dampener module!

--- R/check_reports.R
+++ R/check_reports.R
@@ -1,6 +1,14 @@
 check_reports <- function(x) {
-  diffs <- diff(x)
-  cond1 <- all(diffs > 0) || all(diffs < 0)
-  cond2 <- all(abs(diffs) >= 1) && all(abs(diffs) <= 3)
-  cond1 && cond2
+
+  safety_check <- function(x) {
+    diffs <- diff(x)
+    cond1 <- all(diffs > 0) || all(diffs < 0)
+    cond2 <- all(abs(diffs) >= 1) && all(abs(diffs) <= 3)
+    cond1 && cond2
+  }
+# Problem dampener
+    for (i in seq_along(x)) {
+      if (safe(x[-i])) return(TRUE)
+    }
+    FALSE
 }
